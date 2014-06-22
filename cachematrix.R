ed636ea6f3c69d92208a819d8d46e6a33bb56265
ajinkya-k <kajinkyah@gmail.com>
2014-06-23 01:58:33 +0530

    solved if-cache-present condition

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,15 @@
-## Put comments here that give an overall description of what your
+## This R program caches the inverse of a matrix
 ## functions do
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  invMat <- NULL          ## Sets the default value of the inverse matrix to NULL
+  set <- funvtion(y){
+    invMat <- solve(y)
+    
+  }
+  
 }
 
 
@@ -12,4 +17,9 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  if(!is.null(invMat)){
+    print("Using Inverse stored in Cache")
+    return(invMat)        ## if the cache of the inverse is already present then return it
+  }
+  
 }
